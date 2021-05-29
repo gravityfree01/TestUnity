e824150ec7354a5bc4d9811fed9c88c55e3990a9
DESKTOP-BHJPBEP\gravi <gravityfree01@gmail.com>
2021-05-29 23:37:15 +0900

    숨기와 복원 버튼 활성화 방법 작성
    obj.gameObject.SetActive(true);
    obj.gameObject.SetActive(false);

--- TestUnity/Assets/Script/ClickEvent.cs
+++ TestUnity/Assets/Script/ClickEvent.cs
@@ -7,6 +7,36 @@ public class ClickEvent : MonoBehaviour{
     public GameObject btn1;
     public GameObject btn2;
 
+    private GameObject obj=null;
+
+
+        
+    private void Start()
+    {
+        if(FindObject())
+        {
+            Debug.Log("Find Object");
+
+        }
+        else
+        {
+            Debug.Log("Not Found");
+        }
+
+        
+    }
+
+
+    private bool  FindObject()
+    {
+        if (obj == null){
+            obj = GameObject.Find("Obj").gameObject;
+            return true;
+        }
+        return false;
+
+    }
+
     // 21-05-28 박성수
     // 1번 버트 클릭할때
     // 2번 버튼 활성화, 1번버튼 비활성화
@@ -14,12 +44,15 @@ public class ClickEvent : MonoBehaviour{
     {
         btn2.GetComponent<Button>().interactable = true;
         btn1.GetComponent<Button>().interactable = false;
-
         btn2.GetComponent<Image>().color= Color.red;
         btn1.GetComponent<Image>().color = Color.white;
+        
+        // 21-05-29 박성수 작성
+        obj.gameObject.SetActive(false);
     }
 
-
+ 
+    
 
     // 21-05-28 박성수
     // 2번 버트 클릭할때
@@ -29,9 +62,11 @@ public class ClickEvent : MonoBehaviour{
     {
         btn1.GetComponent<Button>().interactable = true;
         btn2.GetComponent<Button>().interactable = false;
-
         btn1.GetComponent<Image>().color = Color.red;
         btn2.GetComponent<Image>().color = Color.white;
+
+        // 21-05-29 박성수 작성
+        obj.gameObject.SetActive(true);
     }
 
 }
